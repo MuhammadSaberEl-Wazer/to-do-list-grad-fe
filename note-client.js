1faa11f346932183e341ad22aba3ce5c5c8c267c
Muhammad Saber <msaber@hemmahq.com>
2025-04-20 08:20:40 +0200

    Upload FE code - graduation project

---
+++ note-client.js
@@ -0,0 +1,53 @@
+const baseUrl = "http://localhost:3000";
+
+async function addNote(noteData) {
+  const response = await fetch(`${baseUrl}/notes`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(noteData),
+  });
+
+  return response;
+}
+
+async function updateNote(noteData) {
+  console.log("11111111111111111",noteData);
+  
+  const response = await fetch(`${baseUrl}/notes`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(noteData),
+  });
+
+  return response;
+}
+
+async function deleteNote(noteId) {
+  const response = await fetch(`${baseUrl}/notes/${noteId}`, {
+    method: "DELETE",
+  });
+
+  return response;
+}
+
+async function getNoteById(noteId) {
+  const response = await fetch(`${baseUrl}/notes/${noteId}`, {
+    method: "GET",
+  });
+
+  return response.json();
+}
+
+async function getNotes(noteTitle) {
+  var url = `${baseUrl}/notes`;
+
+  if (noteTitle) {
+    url += `/?title=${noteTitle}`;
+  }
+  const response = await fetch(url);
+  return response.json();
+}
